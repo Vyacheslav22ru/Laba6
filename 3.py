26cca5974a57f4f4180c4640e1ec0c64167126e8
Vyacheslav22ru <57097036+Vyacheslav22ru@users.noreply.github.com>
2019-12-20 12:01:45 +0700

    Update 3.py

---
+++ 3.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://www.google.com/")
+time.sleep(5)
+textinput = driver.find_element_by_name("q")
+textinput.send_keys("Поиск в Google")
+submit_button = driver.find_element_by_css_selector(".gNO89b")
+submit_button.click()
+time.sleep(10)
+driver.quit()
